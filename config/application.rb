a965477ec8cf82a24b4f5c5e136574a8c0451acd
Greg Gehrich <ggehrich@gmail.com>
2014-07-15 12:23:41 -0700

    iframe

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module Sap3Gg
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # added by gg to use iframe
+    config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
   end
 end
+
