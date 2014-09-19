b2dcc33b2a4c4fb46fc374509af63f3594814c34
Greg Gehrich <ggehrich@gmail.com>
2014-09-19 14:55:57 -0700

    adding story track fields

--- app/models/story.rb
+++ app/models/story.rb
@@ -12,15 +12,15 @@ class Story < ActiveRecord::Base
   before_validation :set_story_track_fields
 
   def set_story_track_fields
-    # self.author_track = true  # save OK when this block is used
-    # self.story_year_track = true
-    # self.story_month_track = true
-    # self.story_date_track = true
+    self.author_track = true  # save OK when this block is used
+    self.story_year_track = true
+    self.story_month_track = true
+    self.story_date_track = true
 
-    self.author_track = (self.raw_author_scrape == self.author ? true : false) # begin, rollback no save when this block is used
-    self.story_year_track = (self.raw_story_year_scrape == self.story_year ? true : false)
-    self.story_month_track = (self.raw_story_month_scrape == self.story_month ? true : false)
-    self.story_date_track = (self.raw_story_date_scrape == self.story_date ? true : false)
+    # self.author_track = (self.raw_author_scrape == self.author ? true : false) # begin, rollback no save when this block is used
+    # self.story_year_track = (self.raw_story_year_scrape == self.story_year ? true : false)
+    # self.story_month_track = (self.raw_story_month_scrape == self.story_month ? true : false)
+    # self.story_date_track = (self.raw_story_date_scrape == self.story_date ? true : false)
   end
 
 end
