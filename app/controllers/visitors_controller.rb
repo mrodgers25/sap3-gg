e75207e63068f850591f0a9aa06d2a9006f5c236
greggehrich <ggehrich@gmail.com>
2014-10-05 08:08:07 -0700

    initial homepage dashboard

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,7 @@
 class VisitorsController < ApplicationController
 
   def index
-    @stories = Story.order("id DESC").first(5)
+    @stories = Story.order("id DESC").first(9)
     @urls = @stories.first.urls
     @images = @urls.first.images
   end
