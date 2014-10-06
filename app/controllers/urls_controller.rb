83f3b850b4ce6cff31bda0bd9f02789877e85b75
greggehrich <ggehrich@gmail.com>
2014-10-06 15:30:07 -0700

    initial homepage dashboard

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -5,7 +5,7 @@ class UrlsController < ApplicationController
   # GET /urls
   # GET /urls.json
   def index
-    @urls = Url.all
+    @urls = Url.order("url_full DESC").all
   end
 
   # GET /urls/1
