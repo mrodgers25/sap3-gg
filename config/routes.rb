90e7d1551ca8d552c43097f146f5e8633221d3b2
greggehrich <ggehrich@gmail.com>
2014-10-08 10:44:48 -0700

    add indexes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   resources :media_infos
 
-  # match 'stories/new/url_show' => 'stories#url_show', via: [:get, :post]  #added by gg
   match 'stories/new' => 'stories#new', via: [:get, :post]  #added by gg
   match 'stories/scrape' => 'stories#scrape', via: [:get, :post]  #added by gg
 
   resources :stories #original from generate
-
   resources :urls
+  resources :images
 
   root to: 'visitors#index'
   get 'products/:id', to: 'products#show', :as => :products
