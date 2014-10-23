fa8e052abf6e363f2d40f2a0f152bab67f63bb21
greggehrich <ggehrich@gmail.com>
2014-10-22 22:13:47 -0700

    csv export

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -12,13 +12,13 @@ class ReportsController < ApplicationController
     stories = Story.includes(:urls => [:images])
 
     CSV.open( file, 'w' ) do |writer|
-      writer << ["Id", "Created","SAP Publish","YY","MM","DD","Tagline","Location","Place Category","Story Category","Author Trk", \
-            "Story Yr Trk","Story Mnth Trk","Story Dt Trk","URL"]
+      writer << ["Id", "Created","SAP Publish","Story Type","YY","MM","DD","Tagline","Location","Place Category","Story Category","Author Trk", \
+            "Story Yr Trk","Story Mnth Trk","Story Dt Trk","URL","Domain"]
       stories.each do |s|
         s.urls.each do |u|
-          writer << [s.id, s.created_at, s.sap_publish_date, s.story_year, s.story_month, s.story_year, s.editor_tagline, \
+          writer << [s.id, s.created_at, s.sap_publish_date, s.story_type, s.story_year, s.story_month, s.story_date, s.editor_tagline, \
                 s.location_code, s.place_category, s.story_category, s.author_track, s.story_year_track, \
-                s.story_month_track, s.story_date_track, u.url_full]
+                s.story_month_track, s.story_date_track, u.url_full, u.url_domain]
         end
       end
     end
