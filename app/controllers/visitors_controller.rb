c3ae03343f2687632a7e318422ab8fb9622170e9
greggehrich <ggehrich@gmail.com>
2014-10-24 18:00:35 -0700

    story limit

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,7 +2,8 @@ class VisitorsController < ApplicationController
 
   def index
 
-    story_limit = Code.find_by(code_key: "LANDING_PAGE_STORY_COUNT").code_value
+    story_limit = Code.find_by(code_key: "LANDING_PAGE_STORY_COUNT").code_value ? \
+      Code.find_by(code_key: "LANDING_PAGE_STORY_COUNT").code_value : 36
     @stories = Story.order("id DESC").limit(36).includes(:urls => [:images]).limit(story_limit)
 
     @stories = @stories.user_location_code(params[:user_location_code]) if params[:user_location_code].present?
