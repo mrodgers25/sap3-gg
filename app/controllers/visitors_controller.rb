258e7a398e7332bd723f2595b5a91f086b0871d9
greggehrich <ggehrich@gmail.com>
2014-10-24 17:55:10 -0700

    add manual enter

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,7 +2,8 @@ class VisitorsController < ApplicationController
 
   def index
 
-    @stories = Story.order("id DESC").limit(36).includes(:urls => [:images]).limit(36)
+    story_limit = Code.find_by(code_key: "LANDING_PAGE_STORY_COUNT").code_value
+    @stories = Story.order("id DESC").limit(36).includes(:urls => [:images]).limit(story_limit)
 
     @stories = @stories.user_location_code(params[:user_location_code]) if params[:user_location_code].present?
     @stories = @stories.user_place_category(params[:user_place_category]) if params[:user_place_category].present?
