1d1da2684faa884adc6c7b7c47ac264a5944cbc9
greggehrich <ggehrich@gmail.com>
2014-11-10 11:23:13 -0800

    story fetch fix

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,13 @@ class VisitorsController < ApplicationController
 
   def index
 
+    story_limit = Code.find_by(code_key: "LANDING_PAGE_STORY_COUNT").code_value unless \
+      Code.find_by(code_key: "LANDING_PAGE_STORY_COUNT").nil?
+    story_limit ||= 36
+    @stories = Story.order("id DESC").where("sap_publish_date is not null").includes(:urls => [:images]).limit(story_limit)
+
+    flash.now.alert = "No Stories found" if @stories.empty?
+
     # database dropdown data
     @location_codes = Code.order("ascii(code_value)").where("code_type = 'LOCATION_CODE'")
     @story_place_types = Code.order("code_value").where("code_type = 'PLACE_CATEGORY'")
@@ -9,13 +16,6 @@ class VisitorsController < ApplicationController
     @stories = @stories.user_location_code(params[:user_location_code]) if params[:user_location_code].present?
     @stories = @stories.user_place_category(params[:user_place_category]) if params[:user_place_category].present?
     @stories = @stories.user_story_category(params[:user_story_category]) if params[:user_story_category].present?
-
-    story_limit = Code.find_by(code_key: "LANDING_PAGE_STORY_COUNT").code_value unless \
-      Code.find_by(code_key: "LANDING_PAGE_STORY_COUNT").nil?
-    story_limit ||= 36
-    @stories = Story.order("id DESC").where("sap_publish_date is not null").includes(:urls => [:images]).limit(story_limit)
-
-    flash.now.alert = "No Stories found" if @stories.empty?
 
     # scheduler data
     @code_for_next_pub = Code.where("code_key = 'NEXT_STORY_PUB_DATETIME'")
