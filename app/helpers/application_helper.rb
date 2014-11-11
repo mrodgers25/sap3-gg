49f616d29cd9fb0933e5771e6e39b998689ab298
greggehrich <ggehrich@gmail.com>
2014-11-11 06:10:35 -0800

    dynamic scrape popovers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,50 +1,94 @@
 module ApplicationHelper
 
-  def show_location_hover
-    location_hover = Code.where("code_type = 'LOCATION_CODE'").pluck("code_key","code_value")
-    # location_hover.each do |key,value|
-    #   puts "(" + key + ")" + value
-    #   # puts "(" + key + ")" + value
-    # end
-  end
-
   def location_options
-    location_options = "<strong>AZ-Phoenix (PHX)</strong>​<br>
-                        ​<strong>CA-San Francisco (SF)</strong>​<br>
-                        ​<strong>CA-Los Angeles (LA)</strong>​<br>
-                        ​<strong>CA-San Diego (SD)</strong>​<br>
-                        ​<strong>DC-Washington (DC)</strong>​<br>
-                        <strong>IL-Chicago (CHI)</strong><br>
-                        ​<strong>LA-New Orleans (NOLA)</strong><br>
-                        ​<strong>MA-Boston (BOS)</strong>​<br>
-                        ​<strong>NV-Las Vegas (NV)</strong>​<br>
-                        ​<strong>NY-New York City (NYC)</strong>​<br>
-                        ​<strong>OR-Portland (PORT)</strong><br>
-                        ​<strong>PA-Philadelphia (PHIL)</strong><br>
-                        ​<strong>TX-Austin (AUST)</strong>​<br>
-                        ​<strong>WA-Seattle (SEA)</strong>​<br>"
+    hover_list_arr = Code.where("code_type = 'LOCATION_CODE' and code_key != ''").pluck("code_key","code_value")
+
+    hover_list = ""
+
+    hover_list_arr.each do |outer|
+      outer.each_with_index do |inner,ndx|
+        # hover_list << "#{ndx}"
+        hover_list << "(" unless ndx == 1
+        hover_list << inner.to_s
+        hover_list << ") "unless ndx == 1
+      end
+      hover_list << "<br>"
+    end
+    return raw hover_list
   end
 
   def place_category_options
-    place_category_options = "​<strong>(A) Attraction</strong>​<br>
-                        ​<strong>(FD) Food & Drink</strong>​<br>
-                        ​<strong>(L) Lodging</strong>​<br>
-                        ​<strong>(SH) Shopping</strong>​<br>
-                        ​<strong>(SR) Service</strong>​<br>
-                        ​<strong>(SP) Sport or Activity</strong>​"
+    hover_list_arr = Code.where("code_type = 'PLACE_CATEGORY' and code_key != ''").pluck("code_key","code_value")
+
+    hover_list = ""
+
+    hover_list_arr.each do |outer|
+      outer.each_with_index do |inner,ndx|
+        # hover_list << "#{ndx}"
+        hover_list << "(" unless ndx == 1
+        hover_list << inner.to_s
+        hover_list << ") "unless ndx == 1
+      end
+      hover_list << "<br>"
+    end
+    return raw hover_list
   end
 
   def story_category_options
-    story_category_options = "<strong>(EP) Editor Picks</strong><br>
-                        ​<strong>(UN) More Unique Than Usual</strong>​<br>
-                        ​<strong>(TL) Top/Best/Coolest Lists</strong>​<br>
-                        ​<strong>(SI) Suggested Itineraries</strong>​<br>
-                        ​<strong>(IA) Industry Awards</strong>​<br>
-                        ​<strong>(RO) Romance</strong>​<br>
-                        ​<strong>(FF) Family Friendly</strong>​<br>
-                        ​<strong>(PF) Pet Friendly</strong>​"
+    hover_list_arr = Code.where("code_type = 'STORY_CATEGORY' and code_key != ''").pluck("code_key","code_value")
 
+    hover_list = ""
+
+    hover_list_arr.each do |outer|
+      outer.each_with_index do |inner,ndx|
+        # hover_list << "#{ndx}"
+        hover_list << "(" unless ndx == 1
+        hover_list << inner.to_s
+        hover_list << ") "unless ndx == 1
+      end
+      hover_list << "<br>"
+    end
+    return raw hover_list
   end
+
+  # def location_options
+  #   location_options = "<strong>AZ-Phoenix (PHX)</strong>​<br>
+  #                       ​<strong>CA-San Francisco (SF)</strong>​<br>
+  #                       ​<strong>CA-Los Angeles (LA)</strong>​<br>
+  #                       ​<strong>CA-San Diego (SD)</strong>​<br>
+  #                       ​<strong>DC-Washington (DC)</strong>​<br>
+  #                       <strong>IL-Chicago (CHI)</strong><br>
+  #                       ​<strong>LA-New Orleans (NOLA)</strong><br>
+  #                       ​<strong>MA-Boston (BOS)</strong>​<br>
+  #                       ​<strong>NV-Las Vegas (NV)</strong>​<br>
+  #                       ​<strong>NY-New York City (NYC)</strong>​<br>
+  #                       ​<strong>OR-Portland (PORT)</strong><br>
+  #                       ​<strong>PA-Philadelphia (PHIL)</strong><br>
+  #                       ​<strong>TX-Austin (AUST)</strong>​<br>
+  #                       ​<strong>WA-Seattle (SEA)</strong>​<br>"
+  # end
+
+  # def place_category_options
+  #   place_category_options = "​<strong>(A) Attraction</strong>​<br>
+  #                       ​<strong>(FD) Food & Drink</strong>​<br>
+  #                       ​<strong>(L) Lodging</strong>​<br>
+  #                       ​<strong>(SH) Shopping</strong>​<br>
+  #                       ​<strong>(SR) Service</strong>​<br>
+  #                       ​<strong>(SP) Sport or Activity</strong>​"
+  # end
+
+  # def story_category_options
+  #   story_category_options = "<strong>(EP) Editor Picks</strong><br>
+  #                       ​<strong>(UN) More Unique Than Usual</strong>​<br>
+  #                       ​<strong>(TL) Top/Best/Coolest Lists</strong>​<br>
+  #                       ​<strong>(SI) Suggested Itineraries</strong>​<br>
+  #                       ​<strong>(IA) Industry Awards</strong>​<br>
+  #                       ​<strong>(RO) Romance</strong>​<br>
+  #                       ​<strong>(FF) Family Friendly</strong>​<br>
+  #                       ​<strong>(PF) Pet Friendly</strong>​"
+
+  # end
+
 
   # added these three methods for devise access by login modal
   def resource_name
--- test_scripts/nest_arr.rb
+++ test_scripts/nest_arr.rb
@@ -2,14 +2,22 @@ ENV['RAILS_ENV'] ||= 'development'
 require File.expand_path('../../config/environment', __FILE__)
 
 
-location_hover = Code.where("code_type = 'LOCATION_CODE'").pluck("code_key","code_value")
+location_hover_arr = Code.where("code_type = 'LOCATION_CODE' and code_key != ''").pluck("code_key","code_value")
 
+location_hover_list = ""
 
-# a = location_hover.each do |outer|
-#   # outer.each do |inner|
-#     puts "#{outer} "
-#   # end
-# end
+location_hover_arr.each do |outer|
+  outer.each_with_index do |inner,ndx|
+    # location_hover_list << "#{ndx}"
+    location_hover_list << "(" unless ndx == 1
+    location_hover_list << inner.to_s
+    location_hover_list << ") "unless ndx == 1
+  end
+  location_hover_list << "\n"
+end
 
-a = location_hover.each { |outer| puts "#{outer.to_s}" }
+# a = location_hover.each { |outer| puts "#{outer.to_s}" }
 
+puts "location hover is #{location_hover_arr}"
+
+puts "location_hover_list is #{location_hover_list}"
