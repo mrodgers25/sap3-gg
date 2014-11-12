151f8b1a02b8b9cb2cba68f50cdaaf287537af90
greggehrich <ggehrich@gmail.com>
2014-11-11 16:21:18 -0800

    fix modal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,25 +4,25 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # include ApplicationHelper
 
-  # # begin devise additional fields
-  # before_action :configure_devise_permitted_parameters, if: :devise_controller?
-  #
-  # protected
-  #
-  # def configure_devise_permitted_parameters
-  #   registration_params = [:first_name, :last_name, :city_preference, :email, :password, :password_confirmation]
-  #
-  #   if params[:action] == 'update'
-  #     devise_parameter_sanitizer.for(:account_update) {
-  #         |u| u.permit(registration_params << :current_password)
-  #     }
-  #   elsif params[:action] == 'create'
-  #     devise_parameter_sanitizer.for(:sign_up) {
-  #         |u| u.permit(registration_params)
-  #     }
-  #   end
-  # end
-  # # end devise additional fields
+  # begin devise additional fields
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_devise_permitted_parameters
+    registration_params = [:first_name, :last_name, :city_preference, :email, :password, :password_confirmation]
+
+    if params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_update) {
+          |u| u.permit(registration_params << :current_password)
+      }
+    elsif params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) {
+          |u| u.permit(registration_params)
+      }
+    end
+  end
+  # end devise additional fields
 
   private
 
