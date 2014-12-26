3d296a7ffbae3e0d5141b04650eca9ca5a8811a7
greggehrich <ggehrich@gmail.com>
2014-12-26 10:02:55 -0800

    prod mediaowner

--- loadm.rb
+++ loadm.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-csv_text = File.read('media.csv')
+csv_text = File.read('media_prod.csv')
 csv = CSV.parse(csv_text, :headers => true)
 csv.each do |row|
   Mediaowner.create!(row.to_hash)
