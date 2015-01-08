32d9d343975de2c0b73251c3c21f417da7bcef62
greggehrich <ggehrich@gmail.com>
2015-01-08 14:09:26 -0800

    display domain name

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -118,8 +118,8 @@ class StoriesController < ApplicationController
     @url1 = @story.urls.first  # url fields
     @source_url_pre = @url1.url_full
     @base_domain = @url1.url_domain
-    @name_display = Mediaowner.where(url_domain: @base_domain).first.owner_name
-    @name_display = @name_display.present? ? @name_display : 'NO DOMAIN NAME FOUND'
+    @name_display = Mediaowner.where(url_domain: @base_domain).first.owner_name.present? ? Mediaowner.where(url_domain: @base_domain).first.owner_name.present? : 'NO DOMAIN NAME FOUND'
+    # @name_display = @name_display.present? ? @name_display : 'NO DOMAIN NAME FOUND'
     @title = @url1.url_title
     @title_complete = (@title.present? ? 'complete' : 'incomplete')
     @meta_desc = @url1.url_desc
