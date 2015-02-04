b17eb38a23bc5ab52d8f397dfcd4889c6982a89e
ben wanicur <bwanicur@gmail.com>
2015-02-04 10:02:56 -0800

    redirect to encoded url

--- app/controllers/outbound_clicks_controller.rb
+++ app/controllers/outbound_clicks_controller.rb
@@ -1,10 +1,10 @@
 class OutboundClicksController < ApplicationController
   
   def show
-    my_url = URI.decode(params[:url])
+    decoded_url = URI.decode(params[:url])
     my_user_id = current_user ? current_user.id : nil
-    OutboundClick.create(user_id: my_user_id, url: my_url)
-    redirect_to my_url
+    OutboundClick.create(user_id: my_user_id, url: decoded_url)
+    redirect_to params[:url]
   end
   
 end
