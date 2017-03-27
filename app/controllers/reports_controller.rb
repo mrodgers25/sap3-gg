ef033a818f7c4d28434f84910c6e2b813021050f
[01;31m[K"name"[m[K: "Martha Rodgers <[01;31m[K"email"[m[K: "mrodgers25@gmail.com>
2017-03-27 16:30:05 -0700

    Changed to V3 of SendGrid email

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -96,7 +96,7 @@ puts "******Email is #{logged_in_user_email}*****"
 
     #client = SendGrid::Client.new(api_user: ENV["SENDGRID_USERNAME"], api_key: ENV["SENDGRID_PASSWORD"])
 
-client = SendGrid::API.new(api_key: ENV['SENDGRID_PASSWORD'])
+#client = SendGrid::API.new(api_key: ENV['SENDGRID_PASSWORD'])
 
 #response = sg.client.mail._('send').post(request_body: mail.to_json)
 #NEXT SECTION IS A TEST
@@ -106,44 +106,46 @@ client = SendGrid::API.new(api_key: ENV['SENDGRID_PASSWORD'])
 #content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
 #mail = Mail.new('StoriesAboutPlaces.com', 'TEST', '#{logged_in_user_email}', 'Your latest export files are attached.')
 
-#sg = SendGrid::API.new(api_key: ENV['SENDGRID_PASSWORD'])
-#data = JSON.parse('{
-#  "personalizations": [
-#    {
-#      "to": [
-#        {
-#          "email": "mrodgers25@gmail.com"
-#        }
-#      ],
-#      "subject": "Hello World from the SendGrid Ruby Library!"
-#    }
-#  ],
-#  "from": {
-#    "email": "mrodgers@storiesaboutplaces.com"
-#  },
-#  "content": [
-#    {
-#      "type": "text/plain",
- #     "value": "Hello, Email!"
-#    }
-#  ]
-#}')
-#response = sg.client.mail._("send").post(request_body: data)
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_PASSWORD'])
+data = JSON.parse('{
+  "personalizations": [
+    {
+      "to": [
+        {
+          "email": "mrodgers25@gmail.com"
+        }
+      ],
+      "subject": "Hello World from the SendGrid Ruby Library!"
+    }
+  ],
+  "from": {
+    "email": "mrodgers@storiesaboutplaces.com"
+  },
+  "content": [
+    {
+      "type": "text/plain",
+      "value": "Hello, Email!"
+    }
+  ]
+}')
+response = sg.client.mail._("send").post(request_body: data)
+puts response.status_code
+puts response.body
+puts response.headers
 
-
-    mail = SendGrid::Mail.new do |m|
-      m.to = 'mrodgers25@gmail.com'
-      m.from = 'mrodgers@StoriesAboutPlaces.com'
-      m.subject = 'Export of all Stories, Users and Actions'
-      m.txt = 'Your latest export files are attached.'
-    end
+    #mail = SendGrid::Mail.new do |m|
+    #  m.to = 'mrodgers25@gmail.com'
+    #  m.from = 'mrodgers@StoriesAboutPlaces.com'
+    #  m.subject = 'Export of all Stories, Users and Actions'
+    #  m.txt = 'Your latest export files are attached.'
+    #end
 
     #mail.add_attachment("#{file_s}")
     #mail.add_attachment("#{file_u}")
     #mail.add_attachment("#{file_a}")
     #mail.add_attachment("#{file_o}")
 
-    puts client.send(mail)
+    #puts client.send(mail)
     #client.send(mail)
 
     redirect_to :back, notice: "Exports created and sent. They should arrive in about 10 minutes at #{logged_in_user_email}"
