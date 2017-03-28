e92945073c1203c86dc21d636e75326f44ef2c08
[01;31m[K"name"[m[K: "Martha Rodgers <[01;31m[K"email"[m[K: "mrodgers25@gmail.com>
2017-03-27 18:07:55 -0700

    add error checking

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,13 +99,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'storiesaboutplaces.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
       # address: "smtp.gmail.net",
       address: "smtp.sendgrid.net",
-      port: "587",
-      domain: "sendgrid.com",
+      port: 587,
+      domain: "storiesaboutplaces.com",
       #:domain               =>  'heroku.com',
       authentication: :plain,
       enable_starttls_auto: true,
