406f69dc5309b7deb4225da6d660f45ef32ad9cb
[01;31m[K"name"[m[K: "Martha Rodgers <[01;31m[K"email"[m[K: "mrodgers25@gmail.com>
2017-03-27 18:17:09 -0700

    Trial gmail sentd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,16 +102,17 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-      # address: "smtp.gmail.net",
-      address: "smtp.sendgrid.net",
+      address: "smtp.gmail.net",
+      #address: "smtp.sendgrid.net",
       port: 587,
-      domain: "storiesaboutplaces.com",
-      #:domain               =>  'heroku.com',
+      #domain: "storiesaboutplaces.com",
+      domain:               'gmail.com',
       authentication: :plain,
       enable_starttls_auto: true,
-      # user_name: ENV["GMAIL_USERNAME"],
-      user_name: ENV["SENDGRID_USERNAME"],
-      # password: ENV["GMAIL_PASSWORD"]
-      password: ENV["SENDGRID_PASSWORD"]
+      user_name: 'mrodgers25@gmail.com',
+      #user_name: ENV["SENDGRID_USERNAME"],
+      password: 'MarMail24'
+      #password: ENV["SENDGRID_PASSWORD"]
   }
+
 end
