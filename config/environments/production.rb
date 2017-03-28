38230bc9577bf831ccd2d9a894667009da05fdfa
[01;31m[K"name"[m[K: "Martha Rodgers <[01;31m[K"email"[m[K: "mrodgers25@gmail.com>
2017-03-27 17:53:30 -0700

    Changed domain for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,7 +105,7 @@ Rails.application.configure do
       # address: "smtp.gmail.net",
       address: "smtp.sendgrid.net",
       port: "587",
-      domain: "storiesaboutplaces.com",
+      domain: "sendgrid.com",
       #:domain               =>  'heroku.com',
       authentication: :plain,
       enable_starttls_auto: true,
