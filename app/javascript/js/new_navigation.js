d188076702954e87c8bbe7ae4138b1ea3997114e
kekoa428 <thomaskekoafarr@gmail.com>
2020-12-29 21:49:28 -0800

    new logo and styling

--- app/javascript/js/new_navigation.js
+++ app/javascript/js/new_navigation.js
@@ -1,7 +1,6 @@
 document.addEventListener("turbolinks:load", function () {
   $("#menu-toggle").on('click', function (e) {
     e.preventDefault();
-    console.log('clicked');
     $("#wrapper").toggleClass("toggled");
   });
 })
