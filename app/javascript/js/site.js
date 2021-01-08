fc998b5601d0f23ca38add2d25d64949d3bc994b
kekoa428 <thomaskekoafarr@gmail.com>
2021-01-07 20:26:45 -0800

    grid styling

--- app/javascript/js/site.js
+++ app/javascript/js/site.js
@@ -5,12 +5,18 @@ document.addEventListener("turbolinks:load", function () {
     trigger: 'hover'
   });
 
-  //    function refreshTimer(){
-  //        $.ajax({
-  //            url: '/visitors/refresh_timer',
-  //            format: 'js'
-  //        })
-  //    }
+  var grid = document.querySelector('.grid');
+  grid.addEventListener('click', function (event) {
+    // don't proceed if item was not clicked on
+    if (!matchesSelector(event.target, '.grid-item')) {
+      return;
+    }
+    // change size of item via class
+    event.target.classList.toggle('grid-item--gigante');
+    // trigger layout
+    msnry.layout();
+  });
+
 });
 
 // function refreshTimer() {
