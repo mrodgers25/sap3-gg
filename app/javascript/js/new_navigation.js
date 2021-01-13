9e6fc7d7a262526bc5c729253efa2e10e224ffc9
kekoa428 <thomaskekoafarr@gmail.com>
2021-01-12 20:43:34 -0800

    more styling on grid, imagesLoaded library

--- app/javascript/js/new_navigation.js
+++ app/javascript/js/new_navigation.js
@@ -4,6 +4,20 @@ document.addEventListener("turbolinks:load", function () {
     $("#wrapper").toggleClass("toggled");
   });
 
+  // Sizing once images are loaded
+  // var grid = document.querySelector('.story-grid');
+  // imagesLoaded(grid, { background: true }, function () {
+  //   console.log('#container background image loaded');
+  //   var msnry = new Masonry(grid, {
+  //     itemSelector: ".grid-item",
+  //     columnWidth: ".grid-sizer",
+  //     gutter: ".gutter-sizer",
+  //     percentPosition: true,
+  //     horizontalOrder: true,
+  //     resize: true
+  //   });
+  // });
+
   var grid = document.querySelector('.story-grid');
   var msnry = new Masonry(grid, {
     itemSelector: ".grid-item",
