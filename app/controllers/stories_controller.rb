6f88a86bb266c3d5f6207ad79553e94f8cecf676
David <dvcv1992@gmail.com>
2021-04-16 01:48:01 -0700

    Update homepage for video stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -39,8 +39,7 @@ class StoriesController < ApplicationController
     @place_categories = PlaceCategory.order(:name)
     @story_categories = StoryCategory.order(:name)
 
-    @published_items = PublishedItem.joins("
-      INNER JOIN stories ON (publishable_type = 'Story' AND stories.id = publishable_id)
+    @stories = Story.joins("
       LEFT JOIN stories_users ON stories_users.story_id = stories.id
       LEFT JOIN story_locations ON story_locations.story_id = stories.id
       LEFT JOIN locations ON locations.id = story_locations.location_id
@@ -48,14 +47,14 @@ class StoriesController < ApplicationController
       LEFT JOIN place_categories ON place_categories.id = story_place_categories.place_category_id
       LEFT JOIN story_story_categories ON story_story_categories.story_id = stories.id
       LEFT JOIN story_categories ON story_categories.id = story_story_categories.story_category_id
-    ").select('published_items.*, stories.*, stories_users.created_at AS save_date')
-    @published_items = @published_items.where(stories_users: { user_id: current_user.id })
-    @published_items = @published_items.where(locations: { id: params[:location_id] }) if params[:location_id].present?
-    @published_items = @published_items.where(place_categories: { id: params[:place_category_id] }) if params[:place_category_id].present?
-    @published_items = @published_items.where(story_categories: { id: params[:story_category_id] }) if params[:story_category_id].present?
-    @published_items = @published_items.order('stories_users.created_at ASC').distinct
+    ").select('stories.*, stories_users.created_at AS save_date')
+    @stories = @stories.where(stories_users: { user_id: current_user.id })
+    @stories = @stories.where(locations: { id: params[:location_id] }) if params[:location_id].present?
+    @stories = @stories.where(place_categories: { id: params[:place_category_id] }) if params[:place_category_id].present?
+    @stories = @stories.where(story_categories: { id: params[:story_category_id] }) if params[:story_category_id].present?
+    @stories = @stories.order('stories_users.created_at ASC').distinct
 
-    @pagy, @published_items = pagy(@published_items)
+    @pagy, @stories = pagy(@stories)
 
     render layout: "application"
   end
--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -17,12 +17,28 @@ module StoriesHelper
     end
   end
 
-  def admin_story_path(story)
+  def admin_story_path_helper(story)
     case story.type
     when 'MediaStory'
       admin_media_story_path(story)
     when 'VideoStory'
       admin_video_story_path(story)
+    end
+  end
+
+  def show_visual_media(story)
+    case story.type
+    when 'MediaStory'
+      if story.latest_image
+        link_to image_pack_tag('default-image.jpeg', onload: "const mainContentWidth = document.getElementById('main-content').offsetWidth; const divWidth = document.getElementById('grid-card-#{published_item.id}').offsetWidth; const ratio = (divWidth / #{story.image_width.to_f}); const newHeight = (ratio * #{story.image_height.to_f}); this.style.height=newHeight+'px';", class: "grid-image"), story_path(story.permalink), target: '_blank', id: "default-image-#{published_item.id}"
+        link_to image_tag(story.latest_image.src_url, onload: "this.style.display='inline'; document.getElementById('default-image-#{published_item.id}').style.display='none';", class: "grid-image", style: 'display: none;'), story_path(story.permalink), target: '_blank'
+      else
+      link_to image_pack_tag('default-image.jpeg', class: "grid-image"), story_path(story.permalink), target: '_blank'
+      end
+    when 'VideoStory'
+      content_tag :div, class: "p-3" do
+        get_video_iframe(story.latest_url.url_full, width = "100%", height = "100%")
+      end
     end
   end
 end
