717f65be42d87657e4ee0d91ac228e36dcb3e046
kekoa428 <thomaskekoafarr@gmail.com>
2021-04-27 22:15:23 -0700

    Fix story images in cards

--- app/javascript/js/masonry_setup.js
+++ app/javascript/js/masonry_setup.js
@@ -1,20 +1,8 @@
 document.addEventListener("turbolinks:load", function () {
   // Sizing once images are loaded
-  // var grid = document.querySelector('.story-grid');
-  // imagesLoaded(grid, { background: true }, function () {
-  //   console.log('#container background image loaded');
-  //   var msnry = new Masonry(grid, {
-  //     itemSelector: ".grid-item",
-  //     columnWidth: ".grid-sizer",
-  //     gutter: ".gutter-sizer",
-  //     percentPosition: true,
-  //     horizontalOrder: true,
-  //     resize: true
-  //   });
-  // });
-
   var grid = document.querySelector('.story-grid');
-  if (grid) {
+  imagesLoaded(grid, { background: true }, function () {
+    console.log('#container background image loaded');
     var msnry = new Masonry(grid, {
       itemSelector: ".grid-item",
       columnWidth: ".grid-sizer",
@@ -23,5 +11,17 @@ document.addEventListener("turbolinks:load", function () {
       horizontalOrder: true,
       resize: true
     });
-  }
+  });
+
+  // var grid = document.querySelector('.story-grid');
+  // if (grid) {
+  //   var msnry = new Masonry(grid, {
+  //     itemSelector: ".grid-item",
+  //     columnWidth: ".grid-sizer",
+  //     gutter: ".gutter-sizer",
+  //     percentPosition: true,
+  //     horizontalOrder: true,
+  //     resize: true
+  //   });
+  // }
 })
