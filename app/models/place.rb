806d9804a03c997e7111bd5fbf4af0c8fde19012
Alisha Evans <alisha@notch8.com>
2021-06-24 12:15:19 -0700

    wip

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,7 +3,7 @@ class Place < ApplicationRecord
   accepts_nested_attributes_for :address
 
   belongs_to :place_status_option
-  has_many :stories
+  # has_many :stories
 
   validates :name, presence: true
   validates :place_status_option_id, presence: true
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Place, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_place) { FactoryBot.create(:place, address: {locality: 'San Diego, CA'}, place_status_option_id: 1) }
+
+  it 'is valid with valid attributes' do
+    expect(valid_place).to be_valid
+  end
+
+  it 'is not valid without a name' do
+
+  end
+
+  it 'is not valid without a place status option id' do
+
+  end
 end
