f95203c6bfa92e8837f05a080f004fd5cb791d33
Alisha Evans <alisha@notch8.com>
2021-06-24 17:20:55 -0700

    include warden as a rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,7 @@ RSpec.configure do |config|
 
   config.use_transactional_fixtures = false
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Warden::Test::Helpers
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
