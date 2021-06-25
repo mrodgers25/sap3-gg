b8ad6a2b056e9b46aba9b790b32e7bfd396ea9eb
Alisha Evans <alisha@notch8.com>
2021-06-24 18:14:15 -0700

    rubocop fixes

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe Address, type: :model do
+# This empty block triggers a rubocop error so I'm commenting it out
+# until we write specs
 
-end
+# RSpec.describe Address, type: :model do
+# end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -31,7 +33,6 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-
   config.use_transactional_fixtures = false
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Warden::Test::Helpers
@@ -58,11 +59,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
 
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
-      with.test_framework :rspec
-      with.library :rails
-    end
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
   end
 end
