697189939ef75f00a9bb24f9dc9f5c536dc60e5c
Alisha Evans <alisha@notch8.com>
2021-06-25 11:44:41 -0700

    update place model specs

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,9 +5,9 @@ class Place < ApplicationRecord
   attr_accessor :story_id
 
   belongs_to :address
-  accepts_nested_attributes_for :address, reject_if: :all_blank
-
   belongs_to :place_status_option
+
+  accepts_nested_attributes_for :address, reject_if: :all_blank
 
   has_many :story_places, dependent: :destroy
   has_many :stories, through: :story_places
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -3,11 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe Place, type: :model do
-  it { is_expected.to belong_to :address }
-  it { is_expected.to belong_to :place_status_option }
+  it { is_expected.to belong_to(:address) }
+  it { is_expected.to belong_to(:place_status_option) }
+
+  it { is_expected.to accept_nested_attributes_for(:address) }
+
+  it { is_expected.to have_many(:story_places).dependent(:destroy) }
+  it { is_expected.to have_many(:stories).through(:story_places) }
 
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:place_status_option_id) }
-
-  it { is_expected.to accept_nested_attributes_for :address }
 end
