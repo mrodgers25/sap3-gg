ede31957158fadf1f75b52a05153650ab3af193d
Alisha Evans <alisha@notch8.com>
2021-06-24 18:13:36 -0700

    wip: 2 skipped specs

--- spec/requests/places_spec.rb
+++ spec/requests/places_spec.rb
@@ -1,13 +1,40 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe '/places', type: :request do
+RSpec.describe '/admin/places', type: :request do
   context 'with a logged out user' do
-    it 'renders a successful response' do
-      # TODO(alishaevn): figure out why places_path doesn't exist
-      get '/places'
-      expect(response).to be_successful
+    it 'redirects to the log in screen' do
+      get admin_places_path
+      expect(response.status).to eq(302)
     end
   end
 
+  context 'with a logged in user' do
+    let(:user) { FactoryBot.create(:user) }
+
+    before do
+      # TODO(alishaevn): figure out why the user isn't being signed in
+      login_as user
+    end
+
+    xit 'redirects to the log in screen' do
+      get admin_places_path
+      expect(response.status).to eq(302)
+    end
+  end
+
+  context 'with a logged in admin user' do
+    let(:admin_user) { FactoryBot.create(:admin_user) }
+
+    before do
+      # TODO(alishaevn): figure out why the user isn't being signed in
+      login_as admin_user
+    end
+
+    xit 'renders a successful response' do
+      get admin_places_path
+      expect(response).to be_successful
+    end
+  end
 end
