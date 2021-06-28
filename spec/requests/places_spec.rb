62fb165f114a3c927436ca1ce222789f570719a0
Alisha Evans <alisha@notch8.com>
2021-06-28 10:01:02 -0700

    changes for rubocop offenses

--- spec/requests/places_spec.rb
+++ spec/requests/places_spec.rb
@@ -4,10 +4,15 @@ require 'rails_helper'
 
 RSpec.describe '/admin/places', type: :request do
   context 'with a logged out user' do
-    it 'redirects to the log in screen' do
+    before do
       get admin_places_path
+    end
 
+    it 'is inaccessible' do
       expect(response.status).to eq(302)
+    end
+
+    it 'redirects to the log in screen' do
       expect(response.body).to eq('<html><body>You are being <a href="http://www.example.com/users/sign_in">redirected</a>.</body></html>')
     end
   end
@@ -17,12 +22,14 @@ RSpec.describe '/admin/places', type: :request do
 
     before do
       sign_in user
+      get admin_places_path
+    end
+
+    it 'is inaccessible' do
+      expect(response.status).to eq(302)
     end
 
     it 'redirects to the home screen' do
-      get admin_places_path
-
-      expect(response.status).to eq(302)
       expect(response.body).to eq('<html><body>You are being <a href="http://www.example.com/">redirected</a>.</body></html>')
     end
   end
@@ -32,12 +39,14 @@ RSpec.describe '/admin/places', type: :request do
 
     before do
       sign_in admin_user
+      get admin_places_path
     end
 
     it 'renders a successful response' do
-      get admin_places_path
-
       expect(response).to be_successful
+    end
+
+    it 'loads the places page' do
       expect(response.body).to include('<h4><i class="fas fa-map-marker-alt mr-1"></i> Places</h4>')
     end
   end
