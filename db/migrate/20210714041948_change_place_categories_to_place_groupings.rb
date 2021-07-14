9cd52e217532275ab56e8360bcdf2d941bfdf012
kekoa428 <thomaskekoafarr@gmail.com>
2021-07-13 21:49:24 -0700

    Another naming fix for place groupings

--- db/migrate/20210714041948_change_place_categories_to_place_groupings.rb
+++ db/migrate/20210714041948_change_place_categories_to_place_groupings.rb
@@ -2,7 +2,7 @@ class ChangePlaceCategoriesToPlaceGroupings < ActiveRecord::Migration[6.1]
   def change
     rename_table :place_categories, :place_groupings
 
-    rename_table :place_groupings_stories, :place_groupings_stories
-    rename_column :place_groupings_stories, :place_grouping_id, :place_grouping_id
+    rename_table :story_place_categories, :place_groupings_stories
+    rename_column :place_groupings_stories, :place_category_id, :place_grouping_id
   end
 end
