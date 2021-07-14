658ddb9fcb7ad44fa7e51fbf3b2817500d7f7fd9
kekoa428 <thomaskekoafarr@gmail.com>
2021-07-13 21:48:15 -0700

    Fix migration bug

--- db/migrate/20210714041948_change_place_categories_to_place_groupings.rb
+++ db/migrate/20210714041948_change_place_categories_to_place_groupings.rb
@@ -1,6 +1,6 @@
 class ChangePlaceCategoriesToPlaceGroupings < ActiveRecord::Migration[6.1]
   def change
-    rename_table :place_groupings, :place_groupings
+    rename_table :place_categories, :place_groupings
 
     rename_table :place_groupings_stories, :place_groupings_stories
     rename_column :place_groupings_stories, :place_grouping_id, :place_grouping_id
